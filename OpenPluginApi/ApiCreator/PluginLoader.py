008de93ccbcbddfc9c081e5386b93d5fee8345c6
Benjamin Austin <spidertyler1122@gmail.com>
2020-07-26 20:00:45 -0400

    recusive loadFolder Function Added

--- OpenPluginApi/ApiCreator/PluginLoader.py
+++ OpenPluginApi/ApiCreator/PluginLoader.py
@@ -1,3 +1,22 @@
+import os
+def help():
+    message = """
+Loads Plugins
+"""
+    print(message)
+    return message
+
 def load(FileName: str, globals, locals):
     with open(FileName) as f:
         exec(f.read(),globals, locals)
+
+def loadFolder(FolderName: str, globals, locals, recursive = False, parent = ""):
+    files = os.listdir(parent+FolderName)
+    print(parent)
+    for file in files:
+        print(file)
+        if ".py" in file:
+            with open(parent+FolderName+'/'+file) as f:
+                exec(f.read(),globals, locals)
+        elif recursive == True:
+            loadFolder(file, globals, locals, recursive = False, parent = parent+FolderName+'/')
