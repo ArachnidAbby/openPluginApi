34bf86f72133d1b395688484ebcb6b59711c0262
Benjamin Austin <spidertyler1122@gmail.com>
2020-07-26 20:11:23 -0400

    Made Recusive Plugin loading more simple

--- OpenPluginApi/ApiCreator/PluginLoader.py
+++ OpenPluginApi/ApiCreator/PluginLoader.py
@@ -10,13 +10,11 @@ def load(FileName: str, globals, locals):
     with open(FileName) as f:
         exec(f.read(),globals, locals)
 
-def loadFolder(FolderName: str, globals, locals, recursive = False, parent = ""):
-    files = os.listdir(parent+FolderName)
-    print(parent)
+def loadFolder(FolderName: str, globals, locals, recursive = False):
+    files = os.listdir(FolderName)
     for file in files:
-        print(file)
         if ".py" in file:
-            with open(parent+FolderName+'/'+file) as f:
+            with open(FolderName+'/'+file) as f:
                 exec(f.read(),globals, locals)
         elif recursive == True:
-            loadFolder(file, globals, locals, recursive = False, parent = parent+FolderName+'/')
+            loadFolder(FolderName+'/'+file, globals, locals, recursive = False)
